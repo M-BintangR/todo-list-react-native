2bd594767c64f5a8d3e80a8168cf89852da3e504
= <muhbintang650@gmail.com>
2023-11-27 00:25:59 +0800

    text inputs react native ✅

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
+import { TextInput } from 'react-native';
 import {  
   StyleSheet,
   Button,
@@ -12,6 +13,7 @@ import {
 export default function App() {
   const [name, setName] = useState('');
   const [person, setPerson] = useState({name: "Bintang", age: 30});
+  const [age, setAge] = useState('');
 
   const changeName = () => {
     setName('Muhammad Bintang');
@@ -22,6 +24,7 @@ export default function App() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.boldText}>My name is {name}</Text>
+        <Text style={styles.boldText}>Years old is {age}</Text>
         <Text style={styles.boldText}>His name is {person.name} and his age is {person.age}</Text>
         <View style={styles.body}>
           <Text style={styles.boldText}>Lorem, ipsum dolor sit <Text>Test</Text> consectetur adipisicing elit. Fuga, ab?</Text>
@@ -29,7 +32,27 @@ export default function App() {
       </View>
 
       <View style={styles.buttonContainer}>
-        <Button title='update data' onPress={changeName}/>
+        <Button title='update data'/>
+      </View>
+
+      <View style={styles.formContainer}>
+        <Text>Masukkan Nama</Text>
+        <TextInput 
+          multiline
+          style={styles.formInput}
+          placeholder='Masukkan Nama'
+          onChangeText={(value) => setName(value)}
+        />
+      </View>
+
+      <View style={styles.formContainer}>
+        <Text>Masukkan Umur</Text>
+        <TextInput 
+          keyboardType='numeric'
+          style={styles.formInput}
+          placeholder='Masukkan Nama'
+          onChangeText={(value) => setAge(value)}
+        />
       </View>
 
     </View>
@@ -57,5 +80,16 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 20,
-  }
+  },
+  formContainer: {
+    padding: 20,
+    margin: 10,
+  },
+  formInput: {
+    borderWidth: 1,
+    borderColor: '#777',
+    padding: 8,
+    margin: 10,
+    width: 200,
+  },
 });
