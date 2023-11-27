4c7c1efd4b3a063d3e10c96f68cf26cd2a29bc2f
= <muhbintang650@gmail.com>
2023-11-27 23:00:25 +0800

    toucheble opacity react native ✅

--- Latihan2.js
+++ Latihan2.js
@@ -1,37 +1,59 @@
 import React, { useState } from 'react'
 
 import {  
+    FlatList,
     ScrollView,
     StyleSheet,
     Text,
+    TouchableOpacity,
     View,
 } from 'react-native';
 
 export default function Latihan2() {
 
     const [people, setPeople] = useState([
-        {name: "Bintang", key: '1'},
-        {name: "Salim", key: '2'},
-        {name: "Fery", key: '3'},
-        {name: "Ayu", key: '4'},
-        {name: "Syahru", key: '5'},
-        {name: "Bayu", key: '6'},
-        {name: "Ilham", key: '7'},
-        {name: "Ilham", key: '8'},
-        {name: "Ilham", key: '9'},
-        {name: "Ilham", key: '10'},
-        {name: "Ilham", key: '11'},
+        {name: "Bintang", id: '1'},
+        {name: "Salim", id: '2'},
+        {name: "Fery", id: '3'},
+        {name: "Ayu", id: '4'},
+        {name: "Syahru", id: '5'},
+        {name: "Bayu", id: '6'},
+        {name: "Ilham", id: '7'},
+        {name: "Ilham", id: '8'},
+        {name: "Ilham", id: '9'},
+        {name: "Ilham", id: '10'},
+        {name: "Ilham", id: '11'},
     ]);
 
 
+    const handlerPress = (id) => {
+         setPeople((prev) => (prev.filter(item => item.id != id)));
+    }
+
   return (
-    <ScrollView style={styles.scrollContainer}>
-        {people.map(item => (
-            <View style={styles.item} key={item.key}>
-                <Text>{item.name}</Text>
-            </View>
-        ))}
-    </ScrollView>
+
+    <View>
+        {/* DENGAN FLATLIST */}
+
+        <FlatList
+            numColumns={2}
+            keyExtractor={item => item.id}
+            data={people}
+            renderItem={({item}) => (
+                <TouchableOpacity onPress={() => handlerPress(item.id)}>
+                    <Text style={styles.item}>{item.name}</Text>
+                </TouchableOpacity>
+            )}
+        />
+
+        {/* <ScrollView style={styles.scrollContainer}>
+            {people.map(item => (
+                <View style={styles.item} key={item.key}>
+                    <Text>{item.name}</Text>
+                </View>
+            ))}
+        </ScrollView> */}
+    </View>
   )
 }
 
@@ -41,7 +63,7 @@ const styles = StyleSheet.create({
         padding: 30,
         marginHorizontal: 20,
         backgroundColor: 'pink',
-        fontSize: 24,
+        fontSize: 15,
     },
     scrollContainer: {
         marginVertical: 20,
